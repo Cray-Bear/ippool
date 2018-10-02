b70407c0daf43f794f692dd31306100ed6f7b7a0
LBear <1798900899@qq.com>
2018-10-02 16:08:21 +0800

    IP信息保存查找

--- src/main/java/com/fty1/ippool/IppoolApplication.java
+++ src/main/java/com/fty1/ippool/IppoolApplication.java
@@ -2,10 +2,13 @@ package com.fty1.ippool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * SpringBoot
+ * @author Cray-Bear
+ * @date 2018-10-02 15:39:05
+ */
 @SpringBootApplication
 public class IppoolApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IppoolApplication.class, args);
 	}
---
+++ src/test/java/com/fty1/ippool/repository/IpInfoRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.fty1.ippool.repository;
+
+import com.alibaba.fastjson.JSON;
+import com.fty1.ippool.IppoolApplicationTests;
+import com.fty1.ippool.entity.IpInfoDO;
+import net.bytebuddy.asm.Advice;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class IpInfoRepositoryTest  extends IppoolApplicationTests {
+
+
+    @Autowired
+    private IpInfoRepository ipInfoRepository;
+
+    @Test
+    public void findIpInfoDOByAddressAndPort() {
+        IpInfoDO ipInfoDO = new IpInfoDO();
+        ipInfoDO.setAddress("127.0.0.1");
+        ipInfoDO.setPort(8080);
+        IpInfoDO re = ipInfoRepository.save(ipInfoDO);
+        System.out.println(JSON.toJSONString(re));
+        IpInfoDO res = ipInfoRepository.findIpInfoDOByAddressAndPort(ipInfoDO.getAddress(), ipInfoDO.getPort());
+        System.out.println(JSON.toJSONString(res));
+
+    }
+}
