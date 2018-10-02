453b5342af78e7538fadbb211ac12aef190fac98
LBear <1798900899@qq.com>
2018-10-02 15:30:23 +0800

    第零阶段-生成的SpringBoot项目

---
+++ src/main/java/com/fty1/ippool/IppoolApplication.java
@@ -0,0 +1,12 @@
+package com.fty1.ippool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IppoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IppoolApplication.class, args);
+	}
+}
