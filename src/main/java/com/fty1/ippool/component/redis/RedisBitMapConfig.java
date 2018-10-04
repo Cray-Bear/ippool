aef551d7b9f3a742d30042c90c363d5c4c814a49
LBear <1798900899@qq.com>
2018-10-04 15:30:25 +0800

    引入Redis，使用BitMap去重

---
+++ src/main/java/com/fty1/ippool/component/redis/RedisBitMapConfig.java
@@ -0,0 +1,15 @@
+package com.fty1.ippool.component.redis;
+
+/**
+ * Redis中Bitmap的所有配置
+ *
+ * @author Crzy-Bear
+ * @date 2018-10-04 11:44 AM
+ **/
+public class RedisBitMapConfig {
+    /**
+     * BitMap：Ip的Hash信息
+     */
+    public static final String BITMAP_UNIQUE_IP_POOL_HASH_CODE = "bitmap.unique.ip.pool.hash.code";
+
+}
--- src/main/java/com/fty1/ippool/service/impl/IpInfoServiceImpl.java
+++ src/main/java/com/fty1/ippool/service/impl/IpInfoServiceImpl.java
@@ -3,17 +3,20 @@ package com.fty1.ippool.service.impl;
 import com.fty1.ippool.IpAddressUtils;
 import com.fty1.ippool.component.rabbitmq.RabbitmqProducer;
 import com.fty1.ippool.component.rabbitmq.RabbitmqQueue;
-import com.fty1.ippool.entity.IpInfoDO;
+import com.fty1.ippool.component.redis.RedisBitMapConfig;
+import com.fty1.ippool.component.redis.RedisManager;
 import com.fty1.ippool.entity.IpInfo;
+import com.fty1.ippool.entity.IpInfoDO;
 import com.fty1.ippool.repository.IpInfoRepository;
 import com.fty1.ippool.service.IpInfoService;
+import com.fty1.ippool.utils.Fty1JsonUtils;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-import java.util.LinkedList;
 
 /**
  * IpInfo服务实现类
@@ -31,6 +34,11 @@ public class IpInfoServiceImpl implements IpInfoService {
 
 
     @Autowired
+    private RedisManager redisManager;
+
+
+
+    @Autowired
     private RabbitmqProducer rabbitmqProducer;
 
     @Override
@@ -40,27 +48,61 @@ public class IpInfoServiceImpl implements IpInfoService {
             log.info("N");
             return;
         }
+
+        /**
+         * 如果Redis里面有这个IP信息就不发送
+         */
+        int hashCode = getHashCode(ipInfo);
+        if(hashCode < 0){
+            log.info("H:{}",ipInfo.toString());
+            return;
+        }
+
+        boolean exists =  redisManager.query(RedisBitMapConfig.BITMAP_UNIQUE_IP_POOL_HASH_CODE,hashCode);
+        if(exists){
+            log.info("E");
+            return;
+        }
+        ipInfo.setUniqueCode(hashCode);
         rabbitmqProducer.producer(RabbitmqQueue.QUEUE_POOL_IP_SOURCE, ipInfo);
+    }
+
+    /**
+     * 根据Ip地址信息获取Hash值
+     * @param ipInfo
+     * @return
+     */
+    private int getHashCode(IpInfo ipInfo) {
+        if(ipInfo == null){
+            return 0;
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(ipInfo.getAddress());
+        stringBuffer.append(ipInfo.getPort());
+        return stringBuffer.toString().hashCode();
     }
 
 
     @Override
     public void ipGather(int n) {
-       while (n > 0) {
-           ipGather();
-           n--;
-       }
+        if (n <= 0) {
+            return;
+        }
+
+        while (n-- > 0) {
+            ipGather();
+        }
     }
 
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public  void saveIpInfo(@NonNull IpInfo info) {
-        IpInfoDO ipInfo = ipInfoRepository.findIpInfoDOByAddressAndPort(info.getAddress(), info.getPort());
-        if (ipInfo != null) {
-            log.info("E");
-            return;
-        }
         IpInfoDO ipInfoDO = IpInfoDO.builder().address(info.getAddress()).createTime(new Date()).port(info.getPort()).build();
         ipInfoRepository.save(ipInfoDO);
+        /**
+         * 写入Redis
+         */
+        redisManager.mark(RedisBitMapConfig.BITMAP_UNIQUE_IP_POOL_HASH_CODE,info.getUniqueCode());
     }
 }
